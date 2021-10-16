5977ea55d0389cfb47d8d58477beeae3af57ce0e
ssunny^^ <sergmenshakov@gmail.com>
2021-10-16 23:20:11 +0500

    Добавьте файлы проекта.

---
+++ TickTackToe/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿
+namespace TickTackToe
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.button6 = new System.Windows.Forms.Button();
+			this.button7 = new System.Windows.Forms.Button();
+			this.button8 = new System.Windows.Forms.Button();
+			this.button9 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.firstWon = new System.Windows.Forms.Label();
+			this.secondWon = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.button10 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// button3
+			// 
+			this.button3.BackColor = System.Drawing.Color.White;
+			this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.button3.FlatAppearance.BorderColor = System.Drawing.Color.LightSkyBlue;
+			this.button3.FlatAppearance.BorderSize = 10;
+			this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 99.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button3.Location = new System.Drawing.Point(362, 35);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(151, 150);
+			this.button3.TabIndex = 2;
+			this.button3.TextAlign = System.Drawing.ContentAlignment.TopRight;
+			this.button3.UseVisualStyleBackColor = false;
+			this.button3.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button4
+			// 
+			this.button4.BackColor = System.Drawing.Color.White;
+			this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.button4.FlatAppearance.BorderColor = System.Drawing.Color.LightSkyBlue;
+			this.button4.FlatAppearance.BorderSize = 10;
+			this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 99.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button4.Location = new System.Drawing.Point(48, 191);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(151, 150);
+			this.button4.TabIndex = 3;
+			this.button4.TextAlign = System.Drawing.ContentAlignment.TopRight;
+			this.button4.UseVisualStyleBackColor = false;
+			this.button4.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button5
+			// 
+			this.button5.BackColor = System.Drawing.Color.White;
+			this.button5.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.button5.FlatAppearance.BorderColor = System.Drawing.Color.LightSkyBlue;
+			this.button5.FlatAppearance.BorderSize = 10;
+			this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 99.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button5.Location = new System.Drawing.Point(205, 191);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(151, 150);
+			this.button5.TabIndex = 4;
+			this.button5.TextAlign = System.Drawing.ContentAlignment.TopRight;
+			this.button5.UseVisualStyleBackColor = false;
+			this.button5.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button6
+			// 
+			this.button6.BackColor = System.Drawing.Color.White;
+			this.button6.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.button6.FlatAppearance.BorderColor = System.Drawing.Color.LightSkyBlue;
+			this.button6.FlatAppearance.BorderSize = 10;
+			this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 99.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button6.Location = new System.Drawing.Point(362, 191);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(151, 150);
+			this.button6.TabIndex = 5;
+			this.button6.TextAlign = System.Drawing.ContentAlignment.TopRight;
+			this.button6.UseVisualStyleBackColor = false;
+			this.button6.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button7
+			// 
+			this.button7.BackColor = System.Drawing.Color.White;
+			this.button7.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.button7.FlatAppearance.BorderColor = System.Drawing.Color.LightSkyBlue;
+			this.button7.FlatAppearance.BorderSize = 10;
+			this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 99.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button7.Location = new System.Drawing.Point(48, 347);
+			this.button7.Name = "button7";
+			this.button7.Size = new System.Drawing.Size(151, 150);
+			this.button7.TabIndex = 6;
+			this.button7.TextAlign = System.Drawing.ContentAlignment.TopRight;
+			this.button7.UseVisualStyleBackColor = false;
+			this.button7.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button8
+			// 
+			this.button8.BackColor = System.Drawing.Color.White;
+			this.button8.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.button8.FlatAppearance.BorderColor = System.Drawing.Color.LightSkyBlue;
+			this.button8.FlatAppearance.BorderSize = 10;
+			this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 99.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button8.Location = new System.Drawing.Point(205, 347);
+			this.button8.Name = "button8";
+			this.button8.Size = new System.Drawing.Size(151, 150);
+			this.button8.TabIndex = 7;
+			this.button8.TextAlign = System.Drawing.ContentAlignment.TopRight;
+			this.button8.UseVisualStyleBackColor = false;
+			this.button8.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button9
+			// 
+			this.button9.BackColor = System.Drawing.Color.White;
+			this.button9.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.button9.FlatAppearance.BorderColor = System.Drawing.Color.LightSkyBlue;
+			this.button9.FlatAppearance.BorderSize = 10;
+			this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 99.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button9.Location = new System.Drawing.Point(362, 347);
+			this.button9.Name = "button9";
+			this.button9.Size = new System.Drawing.Size(151, 150);
+			this.button9.TabIndex = 8;
+			this.button9.TextAlign = System.Drawing.ContentAlignment.TopRight;
+			this.button9.UseVisualStyleBackColor = false;
+			this.button9.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.BackColor = System.Drawing.Color.White;
+			this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.button2.FlatAppearance.BorderColor = System.Drawing.Color.White;
+			this.button2.FlatAppearance.BorderSize = 0;
+			this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 99.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button2.Location = new System.Drawing.Point(205, 35);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(151, 150);
+			this.button2.TabIndex = 1;
+			this.button2.TextAlign = System.Drawing.ContentAlignment.TopRight;
+			this.button2.UseVisualStyleBackColor = false;
+			this.button2.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.Color.White;
+			this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.button1.FlatAppearance.BorderColor = System.Drawing.Color.LightSkyBlue;
+			this.button1.FlatAppearance.BorderSize = 10;
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 99.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button1.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+			this.button1.Location = new System.Drawing.Point(48, 35);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(151, 150);
+			this.button1.TabIndex = 0;
+			this.button1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// firstWon
+			// 
+			this.firstWon.AutoSize = true;
+			this.firstWon.Font = new System.Drawing.Font("Courier New", 39.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.firstWon.Location = new System.Drawing.Point(86, 225);
+			this.firstWon.Name = "firstWon";
+			this.firstWon.Size = new System.Drawing.Size(665, 58);
+			this.firstWon.TabIndex = 9;
+			this.firstWon.Text = "Первый игрок победил\r\n";
+			this.firstWon.Visible = false;
+			// 
+			// secondWon
+			// 
+			this.secondWon.AutoSize = true;
+			this.secondWon.Font = new System.Drawing.Font("Courier New", 39.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.secondWon.Location = new System.Drawing.Point(76, 225);
+			this.secondWon.Name = "secondWon";
+			this.secondWon.Size = new System.Drawing.Size(665, 58);
+			this.secondWon.TabIndex = 10;
+			this.secondWon.Text = "Второй игрок победил";
+			this.secondWon.Visible = false;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Courier New", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label1.Location = new System.Drawing.Point(519, 35);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(264, 27);
+			this.label1.TabIndex = 11;
+			this.label1.Text = "Ходит первый игрок\r\n";
+			// 
+			// button10
+			// 
+			this.button10.BackColor = System.Drawing.Color.Olive;
+			this.button10.FlatAppearance.BorderSize = 0;
+			this.button10.Font = new System.Drawing.Font("Courier New", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button10.ForeColor = System.Drawing.SystemColors.Control;
+			this.button10.Location = new System.Drawing.Point(626, 422);
+			this.button10.Name = "button10";
+			this.button10.Size = new System.Drawing.Size(148, 75);
+			this.button10.TabIndex = 12;
+			this.button10.Text = "Перезапуск";
+			this.button10.UseVisualStyleBackColor = false;
+			this.button10.Click += new System.EventHandler(this.button10_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.White;
+			this.ClientSize = new System.Drawing.Size(800, 522);
+			this.Controls.Add(this.button10);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.secondWon);
+			this.Controls.Add(this.firstWon);
+			this.Controls.Add(this.button9);
+			this.Controls.Add(this.button8);
+			this.Controls.Add(this.button7);
+			this.Controls.Add(this.button6);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.Button button6;
+		private System.Windows.Forms.Button button7;
+		private System.Windows.Forms.Button button8;
+		private System.Windows.Forms.Button button9;
+		private System.Windows.Forms.Label firstWon;
+		private System.Windows.Forms.Label secondWon;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button10;
+	}
+}
+
---
+++ TickTackToe/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TickTackToe
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		public bool firstPlayer = true;
+		public void stopGame()
+		{
+			button1.Enabled = false;
+			button2.Enabled = false;
+			button3.Enabled = false;
+			button4.Enabled = false;
+			button5.Enabled = false;
+			button6.Enabled = false;
+			button7.Enabled = false;
+			button8.Enabled = false;
+			button9.Enabled = false;
+		}
+		public void checkWinFirstPlayer()
+		{
+			if (button1.Text == "x" && button2.Text == "x" && button3.Text == "x")
+			{
+				firstWon.Visible = true;
+				stopGame();
+			}
+			else if (button4.Text == "x" && button5.Text == "x" && button6.Text == "x")
+			{
+				firstWon.Visible = true;
+				stopGame();
+			}
+			else if (button7.Text == "x" && button8.Text == "x" && button9.Text == "x")
+			{
+				firstWon.Visible = true;
+				stopGame();
+			}
+			else if (button1.Text == "x" && button4.Text == "x" && button7.Text == "x")
+			{
+				firstWon.Visible = true;
+				stopGame();
+			}
+			else if (button2.Text == "x" && button5.Text == "x" && button8.Text == "x")
+			{
+				firstWon.Visible = true;
+				stopGame();
+			}
+			else if (button3.Text == "x" && button6.Text == "x" && button9.Text == "x")
+			{
+				firstWon.Visible = true;
+				stopGame();
+			}
+			else if (button1.Text == "x" && button5.Text == "x" && button9.Text == "x")
+			{
+				firstWon.Visible = true;
+				stopGame();
+			}
+			else if (button3.Text == "x" && button5.Text == "x" && button7.Text == "x")
+			{
+				firstWon.Visible = true;
+				stopGame();
+			}
+		}
+		public void checkWinSecondPlayer()
+		{
+			if (button1.Text == "o" && button2.Text == "o" && button3.Text == "o")
+			{
+				secondWon.Visible = true;
+				stopGame();
+			}
+			else if (button4.Text == "o" && button5.Text == "o" && button6.Text == "o")
+			{
+				secondWon.Visible = true;
+				stopGame();
+			}
+			else if (button7.Text == "o" && button8.Text == "o" && button9.Text == "o")
+			{
+				secondWon.Visible = true;
+				stopGame();
+			}
+			else if (button1.Text == "o" && button4.Text == "o" && button7.Text == "o")
+			{
+				secondWon.Visible = true;
+				stopGame();
+			}
+			else if (button2.Text == "o" && button5.Text == "o" && button8.Text == "o")
+			{
+				secondWon.Visible = true;
+				stopGame();
+			}
+			else if (button3.Text == "o" && button6.Text == "o" && button9.Text == "o")
+			{
+				secondWon.Visible = true;
+				stopGame();
+			}
+			else if (button1.Text == "o" && button5.Text == "o" && button9.Text == "o")
+			{
+				secondWon.Visible = true;
+				stopGame();
+			}
+			else if (button3.Text == "o" && button5.Text == "o" && button7.Text == "o")
+			{
+				secondWon.Visible = true;
+				stopGame();
+			}
+		}
+		private void button1_Click(object sender, EventArgs e)
+		{
+			if (firstPlayer)
+			{
+				sender.GetType().GetProperty("Text").SetValue(sender, "x"); 
+				checkWinFirstPlayer();
+				label1.Text = "Ходит второй игрок.";
+			}
+			if (!firstPlayer)
+			{
+				sender.GetType().GetProperty("Text").SetValue(sender, "o");
+				checkWinSecondPlayer();
+				label1.Text = "Ходит первый игрок.";
+			}
+			firstPlayer = !firstPlayer;
+			sender.GetType().GetProperty("Enabled").SetValue(sender, false);
+		}
+
+		private void button10_Click(object sender, EventArgs e)
+		{
+			button1.Enabled = true;
+			button2.Enabled = true;
+			button3.Enabled = true;
+			button4.Enabled = true;
+			button5.Enabled = true;
+			button6.Enabled = true;
+			button7.Enabled = true;
+			button8.Enabled = true;
+			button9.Enabled = true;
+
+			firstWon.Visible = false;
+			secondWon.Visible = false;
+
+			label1.Text = "Ходит первый игрок.";
+
+			button1.Text = "";
+			button2.Text = "";
+			button3.Text = "";
+			button4.Text = "";
+			button5.Text = "";
+			button6.Text = "";
+			button7.Text = "";
+			button8.Text = "";
+			button9.Text = "";
+		}
+	}
+}
